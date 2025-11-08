591de1bfa753c87f79b48b1221ef5211c4fb70f5
shaileshaml <shailesh.mistry.aml@gmail.com>
2025-11-08 18:03:46 +0530

    Create shailesh.php

---
+++ shailesh.php
@@ -0,0 +1,11 @@
+<?php
+            // PHP code starts here
+            $user_name = "Shailesh"; // A simple variable
+            echo "<p>Hello, $user_name!</p>"; // Output a dynamic greeting
+            
+            // Output the current date and time
+            echo '<div class="date-time">';
+            echo 'Current Server Time is: ' . date('h:i:s A, l, F j, Y');
+            echo '</div>';
+            // PHP code ends here
+        ?>
