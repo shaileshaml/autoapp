62d2a253196548a2e2ab084db33a345a599cb7f6
shaileshaml <shailesh.mistry.aml@gmail.com>
2025-11-19 11:07:38 +0530

    Update ip.php

--- ip.php
+++ ip.php
@@ -1,4 +1,6 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
 $data = [ 'ip' =>  $_SERVER['REMOTE_ADDR'] ];
 header('Content-type: application/json');
 echo json_encode( $data );
