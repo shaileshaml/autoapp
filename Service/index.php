a51b940f347f93ad8e83d2a2f986c680496aa964
shaileshaml <shailesh.mistry.aml@gmail.com>
2025-12-27 16:49:29 +0530

    Update index.php

--- Service/index.php
+++ Service/index.php
@@ -1,3 +1,22 @@
 <?php
-echo "Welcome ";
+
+$botToken = "7672249611:AAHOPbCKIOxsUWOG1HmRpMtOq_cS_AVmPO";
+$chatId = "1635904266";
+$message = "Hello from PHP via cURL!";
+
+$url = "https://api.telegram.org/bot$botToken/sendMessage";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, [
+    'chat_id' => $chatId,
+    'text' => $message
+]);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+echo $response;
 ?>
